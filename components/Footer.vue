6c8102da3544e4b52bb8ffb335edb057ae0704e8
Gast <gast@MacBook-Pro-van-Lars.local>
2024-06-12 13:37:05 +0200

    Improved SEO

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,8 +2,8 @@
     <footer class="lg:flex lg:justify-center">
         <p class="text-center my-4">In Diemen, werkzaam door heel Nederland</p>
         <ul class="flex justify-center lg:m-4">
-            <!-- <li class="p-1"><a href=""><img src="/assets/content/insta.png" class="social-icon-img"></a></li> -->
-            <li class="p-1"><a href="https://www.linkedin.com/company/sea-lion-web-design"><img src="/assets/content/linkedin.png" class="social-icon-img"></a></li>
+            <li class="p-1"><a href="https://www.instagram.com/sealionwebdesign/"><img src="/assets/content/insta.png" class="social-icon-img" alt="Icon link to Instagram of Sea Lion Web Design" loading="lazy"></a></li>
+            <li class="p-1"><a href="https://www.linkedin.com/company/sea-lion-web-design"><img src="/assets/content/linkedin.png" class="social-icon-img" alt="Icon link to LinkedIn page of Sea Lion Web Design" loading="lazy"></a></li>
         </ul>
         <!-- <div class="text-center my-4">
             <a href="" class="p-2">Algemene voorwaarden</a>
